44e4b932676c3b934b14fd0bc7030319d470f177
Debjit Saha <debjit255@gmail.com>
2017-03-04 05:53:49 +0100

    1. Remove the WPBeginner RSS dashboard

--- views/settings.php
+++ views/settings.php
@@ -1,57 +1,57 @@
 <div class="wrap">
     <h2><?php echo $this->plugin->displayName; ?> &raquo; <?php _e('Settings', $this->plugin->name); ?></h2>
-           
-    <?php    
+
+    <?php
     if (isset($this->message)) {
         ?>
-        <div class="updated fade"><p><?php echo $this->message; ?></p></div>  
+        <div class="updated fade"><p><?php echo $this->message; ?></p></div>
         <?php
     }
     if (isset($this->errorMessage)) {
         ?>
-        <div class="error fade"><p><?php echo $this->errorMessage; ?></p></div>  
+        <div class="error fade"><p><?php echo $this->errorMessage; ?></p></div>
         <?php
     }
-    ?> 
-    
+    ?>
+
     <div id="poststuff">
     	<div id="post-body" class="metabox-holder columns-2">
     		<!-- Content -->
     		<div id="post-body-content">
-				<div id="normal-sortables" class="meta-box-sortables ui-sortable">                        
+				<div id="normal-sortables" class="meta-box-sortables ui-sortable">
 	                <div class="postbox">
 	                    <h3 class="hndle"><?php _e('Settings', $this->plugin->name); ?></h3>
-	                    
+
 	                    <div class="inside">
 		                    <form action="options-general.php?page=<?php echo $this->plugin->name; ?>" method="post">
 		                    	<p>
 		                    		<label for="ihaf_insert_header"><strong><?php _e('Scripts in Header', $this->plugin->name); ?></strong></label>
-		                    		<textarea name="ihaf_insert_header" id="ihaf_insert_header" class="widefat" rows="8" style="font-family:Courier New;"><?php echo $this->settings['ihaf_insert_header']; ?></textarea>	
-		                    		<?php _e('These scripts will be printed in the <code>&lt;head&gt;</code> section.', $this->plugin->name); ?>	
+		                    		<textarea name="ihaf_insert_header" id="ihaf_insert_header" class="widefat" rows="8" style="font-family:Courier New;"><?php echo $this->settings['ihaf_insert_header']; ?></textarea>
+		                    		<?php _e('These scripts will be printed in the <code>&lt;head&gt;</code> section.', $this->plugin->name); ?>
 		                    	</p>
 		                    	<p>
 		                    		<label for="ihaf_insert_footer"><strong><?php _e('Scripts in Footer', $this->plugin->name); ?></strong></label>
-		                    		<textarea name="ihaf_insert_footer" id="ihaf_insert_footer" class="widefat" rows="8" style="font-family:Courier New;"><?php echo $this->settings['ihaf_insert_footer']; ?></textarea>	
-		                    		<?php _e('These scripts will be printed above the <code>&lt;/body&gt;</code> tag.', $this->plugin->name); ?>	
+		                    		<textarea name="ihaf_insert_footer" id="ihaf_insert_footer" class="widefat" rows="8" style="font-family:Courier New;"><?php echo $this->settings['ihaf_insert_footer']; ?></textarea>
+		                    		<?php _e('These scripts will be printed above the <code>&lt;/body&gt;</code> tag.', $this->plugin->name); ?>
 		                    	</p>
 		                    	<?php wp_nonce_field($this->plugin->name, $this->plugin->name.'_nonce'); ?>
 		                    	<p>
-									<input name="submit" type="submit" name="Submit" class="button button-primary" value="<?php _e('Save', $this->plugin->name); ?>" /> 
+									<input name="submit" type="submit" name="Submit" class="button button-primary" value="<?php _e('Save', $this->plugin->name); ?>" />
 								</p>
 						    </form>
 	                    </div>
 	                </div>
 	                <!-- /postbox -->
-	                
+
 	                <?php
 	                // RSS Feed
 	                if (isset($this->dashboard)) {
 	                	?>
 		                <div id="wpbeginner" class="postbox">
 		                    <h3 class="hndle"><?php _e('Latest from WPBeginner', $this->plugin->name); ?></h3>
-		                    
+
 		                    <div class="inside">
-			                    <?php 
+			                    <?php
 			                    $this->dashboard->outputDashboardWidget();
 								?>
 		                    </div>
@@ -64,12 +64,12 @@
 				<!-- /normal-sortables -->
     		</div>
     		<!-- /post-body-content -->
-    		
+
     		<!-- Sidebar -->
     		<div id="postbox-container-1" class="postbox-container">
-    			<?php require_once($this->plugin->folder.'/_modules/dashboard/views/sidebar-donate.php'); ?>		
+    			<?php require_once($this->plugin->folder.'/views/sidebar-donate.php'); ?>
     		</div>
     		<!-- /postbox-container -->
     	</div>
-	</div>      
+	</div>
 </div>
