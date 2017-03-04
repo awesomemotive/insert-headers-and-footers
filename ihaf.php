caa6d86b2ec775e6c35e00dc2c6c0c8595a487b3
Debjit Saha <debjit255@gmail.com>
2017-03-04 04:31:14 +0100

    Remove the WPBeginner dashborad from Main dashboard

--- ihaf.php
+++ ihaf.php
@@ -12,7 +12,7 @@
 /*  Copyright 2014 WPBeginner
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -42,21 +42,15 @@ class InsertHeadersAndFooters {
         $this->plugin->folder       = plugin_dir_path( __FILE__ );
         $this->plugin->url          = plugin_dir_url( __FILE__ );
 
-        // Dashboard Submodule
-        if (!class_exists('WPBeginnerDashboardWidget')) {
-			require_once($this->plugin->folder.'/_modules/dashboard/dashboard.php');
-		}
-		$this->dashboard = new WPBeginnerDashboardWidget($this->plugin); 
-		
 		// Hooks
 		add_action('admin_init', array(&$this, 'registerSettings'));
         add_action('admin_menu', array(&$this, 'adminPanelsAndMetaBoxes'));
-        
+
         // Frontend Hooks
         add_action('wp_head', array(&$this, 'frontendHeader'));
 		add_action('wp_footer', array(&$this, 'frontendFooter'));
 	}
-	
+
 	/**
 	* Register Settings
 	*/
@@ -64,14 +58,14 @@ class InsertHeadersAndFooters {
 		register_setting($this->plugin->name, 'ihaf_insert_header', 'trim');
 		register_setting($this->plugin->name, 'ihaf_insert_footer', 'trim');
 	}
-	
+
 	/**
     * Register the plugin settings panel
     */
     function adminPanelsAndMetaBoxes() {
     	add_submenu_page('options-general.php', $this->plugin->displayName, $this->plugin->displayName, 'manage_options', $this->plugin->name, array(&$this, 'adminPanel'));
 	}
-    
+
     /**
     * Output the Administration Panel
     * Save POSTed data from the Administration Panel into a WordPress option
@@ -81,50 +75,50 @@ class InsertHeadersAndFooters {
         if (isset($_POST['submit'])) {
         	// Check nonce
         	if (!isset($_POST[$this->plugin->name.'_nonce'])) {
-	        	// Missing nonce	
+	        	// Missing nonce
 	        	$this->errorMessage = __('nonce field is missing. Settings NOT saved.', $this->plugin->name);
         	} elseif (!wp_verify_nonce($_POST[$this->plugin->name.'_nonce'], $this->plugin->name)) {
 	        	// Invalid nonce
 	        	$this->errorMessage = __('Invalid nonce specified. Settings NOT saved.', $this->plugin->name);
-        	} else {        	
+        	} else {
 	        	// Save
 	    		update_option('ihaf_insert_header', $_POST['ihaf_insert_header']);
 	    		update_option('ihaf_insert_footer', $_POST['ihaf_insert_footer']);
 				$this->message = __('Settings Saved.', $this->plugin->name);
 			}
         }
-        
+
         // Get latest settings
         $this->settings = array(
         	'ihaf_insert_header' => stripslashes(get_option('ihaf_insert_header')),
         	'ihaf_insert_footer' => stripslashes(get_option('ihaf_insert_footer')),
         );
-        
+
     	// Load Settings Form
-        include_once(WP_PLUGIN_DIR.'/'.$this->plugin->name.'/views/settings.php');  
+        include_once(WP_PLUGIN_DIR.'/'.$this->plugin->name.'/views/settings.php');
     }
-    
+
     /**
 	* Loads plugin textdomain
 	*/
 	function loadLanguageFiles() {
 		load_plugin_textdomain($this->plugin->name, false, $this->plugin->name.'/languages/');
 	}
-	
+
 	/**
 	* Outputs script / CSS to the frontend header
 	*/
 	function frontendHeader() {
 		$this->output('ihaf_insert_header');
 	}
-	
+
 	/**
 	* Outputs script / CSS to the frontend footer
 	*/
 	function frontendFooter() {
 		$this->output('ihaf_insert_footer');
 	}
-	
+
 	/**
 	* Outputs the given setting, if conditions are met
 	*
@@ -136,20 +130,20 @@ class InsertHeadersAndFooters {
 		if (is_admin() OR is_feed() OR is_robots() OR is_trackback()) {
 			return;
 		}
-		
+
 		// Get meta
 		$meta = get_option($setting);
 		if (empty($meta)) {
 			return;
-		}	
+		}
 		if (trim($meta) == '') {
 			return;
 		}
-		
+
 		// Output
 		echo stripslashes($meta);
 	}
 }
-		
+
 $ihaf = new InsertHeadersAndFooters();
 ?>
