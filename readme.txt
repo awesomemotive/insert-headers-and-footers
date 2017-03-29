147edec671164f5ca9d8d3c8f46118dd94657b93
Debjit Saha <debjit255@gmail.com>
2017-03-29 02:29:10 +0200

    Code cleanups

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: WPbeginner, smub, deb255
 Tags: code, content, css, footer, footer code, footer scripts, footers, google analytics, head, header, header code, header scripts, headers, insert, insert code, insert scripts, js, meta, meta tags, scripts, wpmu
 Requires at least: 3.6
-Tested up to: 4.7.2
+Tested up to: 4.7.3
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
--- views/dashboard-notices.php
+++ views/dashboard-notices.php
@@ -3,18 +3,18 @@
  * Notices template
  */
 ?>
-<div class="notice notice-success is-dismissible <?= $this->plugin->name ?>-notice-welcome">
-	<p>Thank you for installing <?= $this->plugin->displayName ?>! <a href="<?= $setting_page ?>">Click here</a> to configure the plugin.</p>
+<div class="notice notice-success is-dismissible <?php echo $this->plugin->name; ?>-notice-welcome">
+	<p>Thank you for installing <?php echo $this->plugin->displayName; ?>! <a href="<?php echo $setting_page; ?>">Click here</a> to configure the plugin.</p>
 </div>
 <script type="text/javascript">
 	jQuery(document).ready( function($) {
-		$(document).on( 'click', '.<?= $this->plugin->name ?>-notice-welcome button.notice-dismiss', function( event ) {
+		$(document).on( 'click', '.<?php echo $this->plugin->name; ?>-notice-welcome button.notice-dismiss', function( event ) {
 			event.preventDefault();
 			$.post( ajaxurl, {
-				action: '<?= $this->plugin->name . '_dismiss_dashboard_notices' ?>',
-				nonce: '<?= wp_create_nonce( $this->plugin->name . '-nonce' ) ?>'
+				action: '<?php echo $this->plugin->name . '_dismiss_dashboard_notices'; ?>',
+				nonce: '<?php echo wp_create_nonce( $this->plugin->name . '-nonce' ); ?>'
 			});
-			$('.<?= $this->plugin->name ?>-notice-welcome').remove();
+			$('.<?php echo $this->plugin->name; ?>-notice-welcome').remove();
 		});
 	});
 </script>
